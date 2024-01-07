ce9433a227bc6bc2fb0b1c13cd5c9821be75724d
kaigo <r202203404ww@jindai.jp>
2024-01-07 15:23:46 +0900

    forloop

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main(void){
     int a=90;
-    printf("hello.%d.",a);
+    for(int i=0;i<4;i++){
+        a=a+i;
+        printf("now %d.\n",a);
+    }
+    printf("hello.%d.\n",a);
     return 0;
 }
